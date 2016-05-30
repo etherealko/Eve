97e1292ec987f4c56c8d444ec9f4d308e2df1bda
eth <ya@daunautist.ru>
2016-05-30 13:08:46 +0300

    initial ef db model

--- src/eth.Eve.Storage/EveDb.cs
+++ src/eth.Eve.Storage/EveDb.cs
@@ -1,31 +1,26 @@
+using eth.Eve.Storage.Model;
+using System.Data.Entity;
+
 namespace eth.Eve.Storage
 {
-    using System;
-    using System.Data.Entity;
-    using System.Linq;
-
     public class EveDb : DbContext
     {
-        // Your context has been configured to use a 'EveDb' connection string from your application's 
-        // configuration file (App.config or Web.config). By default, this connection string targets the 
-        // 'eth.Eve.Storage.EveDb' database on your LocalDb instance. 
-        // 
-        // If you wish to target a different database and/or database provider, modify the 'EveDb' 
-        // connection string in the application configuration file.
-        public EveDb()
-            : base("name=EveDb")
+        static EveDb()
         {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<EveDb>());
         }
 
-        // Add a DbSet for each entity type that you want to include in your model. For more information 
-        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+        public virtual DbSet<PluginStoreString> PluginStoreStrings { get; set; }
+        public virtual DbSet<PluginStoreBinary> PluginStoreBinaries { get; set; }
+        
+        public EveDb(string nameOrConnectionString = "name=EveDb") : base(nameOrConnectionString) { }
 
-        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PluginStoreString>().HasKey(e => new { e.PluginGuid, e.Key });
+            modelBuilder.Entity<PluginStoreBinary>().HasKey(e => new { e.PluginGuid, e.Key });
+            
+            modelBuilder.Entity<PluginStoreBinary>().HasRequired(e => e.Value);
+        }
     }
-
-    //public class MyEntity
-    //{
-    //    public int Id { get; set; }
-    //    public string Name { get; set; }
-    //}
 }
