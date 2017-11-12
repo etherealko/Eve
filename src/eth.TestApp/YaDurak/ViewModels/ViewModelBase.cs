bfbaf77100c86a161dde05ca2cbac3b50c70dad8
eth <ya@daunautist.ru>
2017-11-12 21:40:44 +0300

    viewmodelbase

--- src/eth.TestApp/YaDurak/ViewModels/ViewModelBase.cs
+++ src/eth.TestApp/YaDurak/ViewModels/ViewModelBase.cs
@@ -5,15 +5,27 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 
 namespace eth.TestApp.YaDurak.ViewModels
 {
     public abstract class ViewModelBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        
+
         [NotifyPropertyChangedInvocator]
-        private void OnPropertyChanged(string propertyName = null)
+        protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value)) 
+                return false;
+            
+            field = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        private void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
