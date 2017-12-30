265a82d8dab0af58487d6f369e0a8b02a20f6220
eth <ya@daunautist.ru>
2017-12-30 13:30:33 +0300

    :(

---
+++ src/eth.EvePlugins.Stickerpacker/Internal/StickerpackerChat.cs
@@ -0,0 +1,17 @@
+﻿using eth.Telegram.BotApi.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eth.Eve.Plugins.Stickerpacker.Internal
+{
+    internal class StickerpackerChat
+    {
+        public StickerpackerChat(Chat chat)
+        {
+
+        }
+    }
+}
---
+++ src/eth.EvePlugins.Stickerpacker/StickerpackerPlugin.cs
@@ -0,0 +1,26 @@
+﻿using eth.Eve.PluginSystem;
+using System;
+
+namespace eth.PluginSamples
+{
+    public sealed class StickerpackerPlugin : IPlugin
+    {
+        private IPluginContext _ctx;
+
+        public PluginInfo Info { get; } = new PluginInfo(new Guid("AA3EC3F1-7864-489C-8334-3D36E1BA9A49"), "Stickerpacker", "Stickerpacker", "0.0.0.1");
+
+        public void Initialize(IPluginContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public void Initialized() { }
+
+        public HandleResult Handle(IUpdateContext c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Teardown() { }
+    }
+}
